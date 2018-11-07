6c1fb5a54d6c01afdbde55783bca2e9cac5a1cf5
David Dooling <david@atomist.com>
2018-11-07 15:24:54 -0600

    Update TS, remove test code coverage
    Ugrade TypeScript to 3.1.6.  Upgrade TypeDoc.  Update TSLint
    configuration.

--- test/empty.test.ts
+++ test/empty.test.ts
@@ -13,3 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import * as assert from "power-assert";
+
+describe("empty", () => {
+    it("is true", () => {
+        assert(true);
+    });
+});
